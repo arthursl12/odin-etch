b11c98126cadde46a5fecd4b1d71223fa958aba6
arthursl12 <arthursoutolima@gmail.com>
2023-01-18 07:43:22 -0300

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-let GRID_SIZE = 16;
+let WIDTH = "700px";
 
-
-function createGrid(){
+function createGrid(size=16){
+    // Define grid total width
     const gridContainer = document.querySelector(".grid-container");
-    for(let i=0; i<GRID_SIZE; i++){
-        for(let j=0; j<GRID_SIZE; j++){
+    gridContainer.style.width = WIDTH;
+
+    // Remove previous grid if existing
+    let children = document.querySelectorAll(".grid-container > .grid-slot");
+    if (children){
+        children.forEach(child => gridContainer.removeChild(child));
+    }
+    
+    // Create grid
+    for(let i=0; i<size; i++){
+        for(let j=0; j<size; j++){
             let slot = document.createElement("div");
             slot.classList.add("grid-slot");
+            
+            
+            let newStr = "calc(("+WIDTH+"/"+String(size)+"))";
+            console.log(newStr);
+
+            slot.style.width = newStr;
             gridContainer.appendChild(slot);
         }
     }
 }
-
-
-
 
 function colorOnHover(e){
     const slot = e.target;
     slot.classList.add("colored-slot");
 }
 
-
 function addHoverEvents(){
     const slots = document.querySelectorAll(".grid-slot");
     slots.forEach(sl => sl.addEventListener('mouseover', colorOnHover));
+}
+
+function promptNewGrid(){
+    console.log("New Grid");
+    let newSize = Number(prompt("New grid size (max. 100)"));
+    console.log(newSize);
+    createGrid(newSize);
+    addHoverEvents();
 }
 
 createGrid();
